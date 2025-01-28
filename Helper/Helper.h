1c93668c2fc51eca6e07b5cf606f8b625da73d02
ramymonazaa <romeo33667@gmail.com>
2025-01-28 22:54:26 +0200

    Check added to helper

--- Helper/Helper.h
+++ Helper/Helper.h
@@ -7,6 +7,7 @@ class Helper{
     static bool isInBoard(int i, int j);
     static bool isEmptySquare(Board& board, int i, int j);
     static bool haveOpponentPiece(Board& board, Color color, int i, int j);
+    static bool haveCheck(Board& board,Color color, int i, int j);
 };
 
 bool Helper::isInBoard(int i, int j){
@@ -21,4 +22,22 @@ bool Helper::isEmptySquare(Board& board, int i, int j){
 bool Helper::haveOpponentPiece(Board& board, Color color, int i, int j){
     if(!isInBoard(i, j) || isEmptySquare(board, i, j)) return false;
     return board.board[i][j]->color != color;
+}
+
+bool Helper::haveCheck(Board &board, Color color, int i, int j){
+     for(int row=0;row<8;row++)
+    {
+        for(int col=0;col<8;col++) {
+            if (!isEmptySquare(board,row,col)&&
+            haveOpponentPiece(board,color,row,col))
+            {
+                for(auto po:board.board[row][col]->moves)
+                {
+                    if(board.board[po.first][po.second]->name=="K")
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
